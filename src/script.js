7acbc87d842fbab34026174bede39f85645be765
Herley David Alpizar <herley.david.alpizar@gmail.com>
2025-06-25 16:22:22 -0600

    LOL

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const text = document.querySelector('.txt-wave');
+//     const textContent = text.innerHTML;
+//     text.innerHTML = textContent.repeat(5); // Repite el contenido para asegurar el efecto de carrusel infinito
+// });
